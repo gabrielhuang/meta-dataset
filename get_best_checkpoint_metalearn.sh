7f294fd4e6de3824ca628d790bed40e8e7b4c68e
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-09-17 16:37:42 +0000

    new scripts

---
+++ get_best_checkpoint_metalearn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export SOURCE=imagenet
+#for MODEL in baseline baselinefinetune prototypical matching maml maml_init_with_proto
+for MODEL in prototypical
+do
+  export EXPNAME=${MODEL}_${SOURCE}
+  python2 -m meta_dataset.analysis.select_best_model \
+    --all_experiments_root=$EXPROOT \
+    --experiment_dir_basenames='' \
+    --restrict_to_variants=${EXPNAME} \
+    --description=best_${EXPNAME}
+done
