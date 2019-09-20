fbc6ac8d679c34bbbd566e12f432896430f8b0e2
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-09-20 19:54:07 +0000

    fix scripts

--- eval_centroid_SOURCE_SINKHORNREG_onlyImageNet.sh
+++ eval_centroid_SOURCE_SINKHORNREG_onlyImageNet.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 trap "exit" INT
-export SOURCE=$1
+export trainEXPNAME=$1
 export SINKHORNREG=$2
+export GINCONFIG=${3:-$1} # ginconfig is samae as trainExpname unless otherwise specified
 for MODEL in centroid
 do
   export trainEXPNAME=${MODEL}_${SOURCE}
   export evalEXPNAME="${MODEL}_${SOURCE}_sinkhornreg_$SINKHORNREG"
   # set BESTNUM to the "best_update_num" field in the corresponding best_....txt
-  export BESTNUM=$(grep best_update_num ${EXPROOT}/best_${EXPNAME}.txt | awk '{print $2;}')
+  export BESTNUM=$(grep best_update_num ${EXPROOT}/best_${trainEXPNAME}.txt | awk '{print $2;}')
   for DATASET in ilsvrc_2012
   do
     python2 -m meta_dataset.train \
       --is_training=False \
       --records_root_dir=$RECORDS \
       --summary_dir=${EXPROOT}/summaries/${evalEXPNAME}_eval_$DATASET \
-      --gin_config=meta_dataset/learn/gin/best/${trainEXPNAME}.gin \
+      --gin_config=meta_dataset/learn/gin/best/${GINCONFIG}.gin \
       --gin_bindings="LearnerConfig.experiment_name='${evalEXPNAME}'" \
       --gin_bindings="LearnerConfig.pretrained_checkpoint=''" \
       --gin_bindings="LearnerConfig.checkpoint_for_eval='${EXPROOT}/checkpoints/${trainEXPNAME}/model_${BESTNUM}.ckpt'" \
