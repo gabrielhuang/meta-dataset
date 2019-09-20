ee6597acd1f1ff1eae1624cc2af170eea0a146b5
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-09-20 03:00:55 +0000

    fixx typo

--- get_best_checkpoint_MODEL_SOURCE.sh
+++ get_best_checkpoint_MODEL_SOURCE.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 export MODEL=$1
 export SOURCE=$2
-do
-  export EXPNAME=${MODEL}_${SOURCE}
-  python2 -m meta_dataset.analysis.select_best_model \
-    --all_experiments_root=$EXPROOT \
-    --experiment_dir_basenames='' \
-    --restrict_to_variants=${EXPNAME} \
-    --description=best_${EXPNAME}
-done
+export EXPNAME=${MODEL}_${SOURCE}
+python2 -m meta_dataset.analysis.select_best_model \
+--all_experiments_root=$EXPROOT \
+--experiment_dir_basenames='' \
+--restrict_to_variants=${EXPNAME} \
+--description=best_${EXPNAME}
