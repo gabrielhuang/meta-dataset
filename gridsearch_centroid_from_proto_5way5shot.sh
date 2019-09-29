76fd704dd7057a62d50880669c15cce02fcd622f
Gabriel Huang <gbxhuang@gmail.com>
2019-09-29 01:38:45 +0000

    update all sinkhorn values

--- gridsearch_centroid_from_proto_5way5shot.sh
+++ gridsearch_centroid_from_proto_5way5shot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 trap "exit" INT
-for sinkhornreg in 1 3 10 30 100 300 1000 3000
+for sinkhornreg in 0.1 0.3 1 3 10 30 100 300 1000 3000
 do
 	echo "Evaluating SinkhornReg=${sinkhornreg}"
 	./eval_centroid_trainEXPNAME_SINKHORNREG_optGINCONFIG_onlyimagenet.sh prototypical_imagenet ${sinkhornreg} centroid_imagenet_5way_5shot 5way5shot_centroidFromProto_
